af91b92ddcdb216e6598111b224cbf23811b9793
Luke Lonergan <mynameis2056@yahoo.com>
2025-06-24 15:43:05 -0700

    merge tests

--- gocca_test/base_test.go
+++ gocca_test/base_test.go
@@ -332,3 +332,15 @@ func TestVersionFunctions(t *testing.T) {
 		t.Error("HeaderVersionNumber() returned empty string")
 	}
 }
+
+func TestVersion(t *testing.T) {
+	version := gocca.GetOccaVersion()
+	t.Logf("OCCA Version: %s", version)
+
+	if version != "2.0.0" {
+		t.Errorf("Expected OCCA 2.0.0, got %s", version)
+	}
+
+	info := gocca.GetVersionInfo()
+	t.Logf("Version Info: %s", info)
+}
